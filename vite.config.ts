b72154bf45843e94eec3e218de7cfbbea15cf6da
quakelele <atwarasulov@gmail.com>
2025-05-22 14:52:04 +0400

    fixed.

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-
 export default defineConfig({
    plugins: [react(), tsconfigPaths()],
-   css: { devSourcemap: true },
-   build: {
-     sourcemap: true,
-     minify: 'esbuild', // по умолчанию, можно попробовать отключить minify для теста: minify: false,
-   },
 })
