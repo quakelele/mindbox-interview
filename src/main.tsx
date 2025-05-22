4424e6d1008830bcd4aa58ceb8983a760143e317
quakelele <atwarasulov@gmail.com>
2025-05-22 14:40:15 +0400

    hot fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import 'shared/styles/global.module.scss'
+import './shared/styles/global.module.scss'
 import { App } from 'app/index'
 
 createRoot(document.getElementById('root')!).render(<App />)
