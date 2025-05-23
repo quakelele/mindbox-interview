16c16182905c02625472b47bbe74badfaefc4f54
quakelele <atwarasulov@gmail.com>
2025-05-23 14:20:29 +0400

    main >.<

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,13 +3,11 @@ import style from 'shared/styles/app.module.scss'
 
 export const App: React.FC = () => {
    return (
-      <div
+      <main
          className={style.wrapper}
          title="For delete some todos, u need to doubleclick ;P">
          <h1>Todos!</h1>
          <TodoPage />
-
-        
-      </div>
+      </main>
    )
 }
